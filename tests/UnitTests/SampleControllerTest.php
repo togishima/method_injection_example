fb2b692b4f6324259c129bceb72016ab4c9409be
OGI <65173657+togishima@users.noreply.github.com>
2022-11-26 23:42:15 +0900

    DIコンテナの利用で差し替え

--- tests/UnitTests/SampleControllerTest.php
+++ tests/UnitTests/SampleControllerTest.php
@@ -1,29 +1,18 @@
 <?php
 
+use App\Foundation\DI\Container;
 use App\Http\Controllers\SampleController;
 use Nyholm\Psr7Server\ServerRequestCreatorInterface;
 use PHPUnit\Framework\TestCase;
-use Psr\Http\Message\ResponseFactoryInterface;
-use Psr\Http\Message\StreamFactoryInterface;
 
 class SampleControllerTest extends TestCase
 {
-    private StreamFactoryInterface $streamFactory;
-    private ResponseFactoryInterface $responseFactory;
     private ServerRequestCreatorInterface $creator;
 
     public function setUp():void
     {
-        $psr17Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
-        $creator = new \Nyholm\Psr7Server\ServerRequestCreator(
-                $psr17Factory, // ServerRequestFactory
-                $psr17Factory, // UriFactory
-                $psr17Factory, // UploadedFileFactory
-                $psr17Factory  // StreamFactory
-            );
-        $this->creator = $creator;
-        $this->responseFactory = $psr17Factory;
-        $this->streamFactory = $psr17Factory;
+        $this->creator = Container::instance()->get(ServerRequestCreatorInterface::class);
+        $this->controller = Container::instance()->get(SampleController::class);
     }
 
     /**
@@ -33,11 +22,9 @@ class SampleControllerTest extends TestCase
     {
         // リクエストのモック
         $request = $this->creator->fromArrays(server: ['REQUEST_METHOD' => 'GET'], get: ['id' => '1']);
-        // コントローラーのインスタンス化
-        $controller = new SampleController($this->responseFactory, $this->streamFactory);
         // アクションの実行
-        $response = $controller->index($request);
-
+        $response = $this->controller->index($request);
+        // レスポンスの確認
         $this->assertEquals(
             json_encode([
                 'message' => 'this is index controller',
